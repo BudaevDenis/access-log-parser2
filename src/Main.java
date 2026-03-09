b0612aff394a2f5e6265527f94c7bc5086baef81
BudaevDenis <dabudaev.inno.local>
2026-03-09 16:32:49 +0300

    изменение testif

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Testif.sum2(80,1));
+        System.out.println(Testif.sum2(80,858));
     }
 
 }
--- src/Testif.java
+++ src/Testif.java
@@ -2,7 +2,7 @@ public class Testif {
     public static int sum2(int x, int y){
         int result = x+y;
         if (result>=10 && result<=19)
-            return 20;
+            return 555;
         else
          return result;
     }
